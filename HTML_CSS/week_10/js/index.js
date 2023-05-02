24f556fc52b4392a98231ba76e68218c6a13944d
sajan25805 <sajanrajmainali99@gmail.com>
2023-05-02 19:57:50 +0545

    Week 6

---
+++ HTML_CSS/week_10/js/index.js
@@ -0,0 +1,84 @@
+// const PI = 3.14
+// let radius = 3.14
+
+
+// let circumeference = 2*PI*radius
+
+// console.log(`The circumference of a circle is ${circumeference}`)
+
+
+// let b = prompt("Enter the base of the triangle: ")
+// let h = prompt ("Enter the height of the triangle: ")
+
+// let area = 1/2*b*h
+
+// alert(`The area of the triangle is ${area}`)
+
+// let number = prompt("Enter the number: ")
+// let square = number**2
+// alert(`The square of a number is ${square}`)
+// alert(`The cube of a number is ${number**3}`)
+
+
+// if (number%2==0){
+//     alert(`The number ${number} is even`);
+// }
+// else{
+//     alert(`The number ${number} is odd`);
+
+// }
+
+
+// let age = prompt("Enter the age of a person: ")
+
+// if (age>=18){
+//     alert(`The person having age ${age} can vote`);
+// }
+
+// else{
+//     alert(`The person having age ${age} can't vote`);
+// }
+
+
+// let marks = prompt("Enter the marks: ")
+
+
+// if (marks >=70){
+//     alert("First class marks:")
+// }
+
+// else if(marks <=69 && marks >59){
+//     alert("Second class marks: ")
+// }
+
+// else if (marks <=59 && marks >40){
+//     alert("Third CLass Marks: ")
+// }
+
+// else {
+//     alert("Fail")
+// }
+
+
+// let i;
+
+// for( i=1; i<=100; i++) {
+//     if (i%2==0){
+//         console.log(i)
+//     }
+// }
+
+
+function cube(){
+    let i
+    for(i=1; i<=100; i++){
+        let cube = i**3
+        console.log(`The cube of ${i} is: ${cube}`)
+    }
+}
+
+
+
+cube()
+
+
